a9dedbe36c3b4d34590993b787d766ec67c50119
Brooks <47866488+lamb64@users.noreply.github.com>
2026-03-17 07:21:22 -0500

    Implement duplicate letter check for player input
    Add duplicate letter detection in Wordle game

--- Wordle.py
+++ Wordle.py
@@ -1,4 +1,5 @@
 from colorama import init, Fore, Style
+from collections import Counter
 
 
 def Wordle(Word):
@@ -11,12 +12,22 @@ def Wordle(Word):
         Inp = ""
         z = ""
         t = []
+        
+        
 
         #Ask the player
         while(len(Inp) != 5):
             Inp = input(f"{Fore.BLACK}Enter a 5 letter word: ")
             if len(Inp) !=5:
                 print("Try Again (Hint has to be 5 letters)")
+        
+        #Duplicates
+        counts = Counter(Inp)
+        duplicates = [char for char, count in counts.items() if count > 1]
+        if duplicates == ([char for char, count in (Counter(WordA)).items() if count >1]):
+            print("test")
+
+        #Makes Input of player into a list
         Inp = list(Inp.lower())
 
         #Win
@@ -26,15 +37,19 @@ def Wordle(Word):
         
         #Check for right Place
         for x in range(len(WordA)):
-            t = Inp
-            t.pop(int(x-1))
+            #t.pop(int(x-1))
             if WordA[x] == Inp[x]:
                 z = (f"{z}{Fore.GREEN} {Word[x]}")
-            elif Inp[x] in WordA and Inp[x] not in t:
-                z = f"{z}{Fore.YELLOW}{Inp[x]}"
+            elif Inp[x] in WordA:
+                if duplicates == ([char for char, count in (Counter(WordA)).items() if count >1]):
+                    z = f"{z}{Fore.YELLOW}{Inp[x]}"
+                elif list(Inp[x]) != duplicates:
+                    z = f"{z}{Fore.YELLOW}{Inp[x]}"
+                else:
+                    z = f"{z}{Fore.BLACK}{Inp[x]}"
             elif WordA[x] != Inp[x]:
                 z = f"{z}{Fore.BLACK}{Inp[x]}"
         print(z.replace(" ", ""))
 
 
-print(Wordle("House"))
+print(Wordle("hello"))
